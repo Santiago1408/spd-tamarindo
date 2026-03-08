247a220ae5b13b80d8b16121cb01d3f6e3c03b93
2floramos06 <floramgam@gmail.com>
2026-03-08 16:14:07 -0400

    page de biblioteca de casos

---
+++ app/bibliotecaCasos/page.tsx
@@ -0,0 +1,11 @@
+import BibliotecaCasos from "@/components/layout/BibliotecaCasos";
+
+export default function PaginaBiblioteca() {
+  return (
+    <div className="min-h-screen bg-background-light py-10">
+      
+      <BibliotecaCasos />
+
+    </div>
+  );
+}
--- app/simuladorHuella/page.tsx
+++ app/simuladorHuella/page.tsx
@@ -3,9 +3,7 @@ import SimuladorHuella from "@/components/layout/SimuladorHuella";
 export default function PaginaSimulador() {
   return (
     <div className="min-h-screen bg-background-light py-10">
-      
       <SimuladorHuella />
-
     </div>
   );
 }
