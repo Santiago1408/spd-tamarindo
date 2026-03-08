86a9a41a5b7c3f60bc62a586267244f055911096
2floramos06 <floramgam@gmail.com>
2026-03-08 16:00:03 -0400

    Biblioteca de casos fuera de page simulador de huella

--- app/simuladorHuella/page.tsx
+++ app/simuladorHuella/page.tsx
@@ -1,13 +1,10 @@
-import SimuladorHuella from "@/components/layout/SimuladorHuella";
-import BibliotecaCasos from "@/components/layout/BibliotecaCasos"; 
+import SimuladorHuella from "@/components/layout/SimuladorHuella"; 
 
 export default function PaginaSimulador() {
   return (
     <div className="min-h-screen bg-background-light py-10">
       
       <SimuladorHuella />
-      <div className="max-w-5xl mx-auto my-16 border-t border-gray-200"></div>
-      <BibliotecaCasos />
 
     </div>
   );
